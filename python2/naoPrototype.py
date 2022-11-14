2c84786b2d4c3cba6b71334c3a4ea104d6d3f9c6
ds201014 <ds201014@fhstp.ac.at>
2022-11-14 16:41:57 +0100

    update NAO script

--- python2/naoPrototype.py
+++ python2/naoPrototype.py
@@ -3,7 +3,10 @@ import time
 import calendar
 import time
 import requests
-BASE = "http://192.168.32.1:5000/"
+import os
+import ast
+
+BASE = 'http://172.22.0.1:5000/' 
 
 # Python Image Library
 from PIL import Image #works only in local env
@@ -12,20 +15,20 @@ from naoqi import ALProxy
 #Connection settings
 NAOIP = '192.168.8.105'
 PORT = 9559
-NAME = "nao"
-passwd = "19981"
+NAME = 'nao'
+passwd = '19981'
 
 ## TEST
 tts = 'ALTextToSpeech'
 text = ALProxy(tts, NAOIP, PORT)
-text.say("Testrun")
+text.say('Testrun')
 
 def takePicture(IP, PORT,camera,resolution, colorSpace):
-  """
+  '''
   First get an image from Nao, then show it on the screen with PIL.
-  """
-  camProxy = ALProxy("ALVideoDevice", IP, PORT)
-  videoClient = camProxy.subscribeCamera("python_client",0, resolution, colorSpace, 5)
+  '''
+  camProxy = ALProxy('ALVideoDevice', IP, PORT)
+  videoClient = camProxy.subscribeCamera('python_client',0, resolution, colorSpace, 5)
   t0 = time.time()
 
   # Get a camera image.
@@ -34,7 +37,7 @@ def takePicture(IP, PORT,camera,resolution, colorSpace):
   t1 = time.time()
   
   # Time the image transfer.
-  print "acquisition delay ", t1 - t0
+  print 'acquisition delay ', t1 - t0
   camProxy.unsubscribe(videoClient)
 
   # Now we work with the image returned and save it as a PNG  using ImageDraw
@@ -48,7 +51,7 @@ def takePicture(IP, PORT,camera,resolution, colorSpace):
 
 
   # Create a PIL Image from our pixel array.
-  im = Image.frombytes("RGB", (imageWidth, imageHeight), array)
+  im = Image.frombytes('RGB', (imageWidth, imageHeight), array)
 
   # Save the image.
   current_GMT = time.gmtime()
@@ -58,30 +61,45 @@ def takePicture(IP, PORT,camera,resolution, colorSpace):
   
   global imName
   imName = r'image_' + time_stamp + r'.png'
-  im.save("images/" + imName, "PNG") #Save files with continuative numbers
+  im.save('fileshare' + os.sep + imName, 'PNG') #Save files with continuative numbers
  
 
   
-camera = 0 # 0 = top camera, 1 = bottom camera
+camera = 1 # 0 = top camera, 1 = bottom camera
 resolution = 3 # 0 = QQVGA, 1 = QVGA, 2 = VGA
 colorSpace = 11 # http://doc.aldebaran.com/2-5/family/robots/video_robot.html#cameracolorspace-mt9m114
 naoImage = takePicture(NAOIP, PORT,camera,resolution, colorSpace)
 
 
 #Filler
-text.say("I hope you are having a good day.")
+text.say('I hope you are having a good day.')
 
-#Get response from API if it's a new or known person
-#response = requests.get(BASE + "video/2", {})
-#print(response.json())
-
-#Set action for new person
-#Name recognition
+response_fr = requests.get(BASE + 'facerecognition/' + imName) # .get, .post
+response_ed = requests.get(BASE + 'emotiondetection/' + imName)
+result_ed = response_ed.json()
+result_fr = ast.literal_eval(response_fr.json())
+gender = str(result_ed[u'gender'])
+emotion = str(result_ed[u'dominant_emotion'])
+print(emotion, gender)
+if result_fr['name'] == 'not found':
+    text.say(
+      'Nice to meet you, you look like a beautiful ' + gender \
+      + 'and you seem to be ' + emotion \
+      + 'May I ask for your Name?'
+      )
+    # say your name now: *insert SpeechToText
+    # API Call addName
+else:
+    text.say(
+      'Hey ' + result_fr['name'] + 'long time no see!'\
+      + 'You look rather ' + emotion \
+      + 'today.'
+      )
 
 #Set action for known person
-#text.say("Nice to meet you again.")
+#text.say('Nice to meet you again.')
 #text.say(name)
-#text.say("Hope you are having a good day.")
+#text.say('Hope you are having a good day.')
 
 #Get response from API for gender and mood
 
@@ -93,12 +111,3 @@ text.say("I hope you are having a good day.")
 
 
 #Emotion detection
-
-
-
-
-
-
-
-
-
