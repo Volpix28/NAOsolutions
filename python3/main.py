ca2f7c7be38d25a9eb7a3d217799aa1a740a6c58
KoHias <matthias.kopeinig@gmx.at>
2022-11-17 18:50:44 +0100

    moveFile function added

--- python3/main.py
+++ python3/main.py
@@ -11,6 +11,20 @@ api = Api(app)
 fileshare = os.listdir(r'C:\Users\alexa\Desktop\FH\5. Semester\Projekt\rest_api_tut\fileshare')
 knowledge_base = os.listdir(r'C:\Users\alexa\Desktop\FH\5. Semester\Projekt\rest_api_tut\knowledge_base') # knowledge_base
 
+# Functions
+def moveFile(source, dest):
+    """
+    Move single file from source to destination. 
+    source: filepath + filename 
+    dest: new filepath + (new) filename
+    """
+    try:
+        os.rename(source, dest)
+        print(f'Successfully moved File from \"{source}\" to \"{dest}\"')
+    except FileNotFoundError as err:
+        print(f'ERROR: {err}')
+
+
 class EmotionDetection(Resource): # Inherit from Resource
     def get(self, img_name): #overwrite get()
         obj = DeepFace.analyze(img_path = f'img/{img_name}', actions = ['gender', 'emotion'])
@@ -30,13 +44,13 @@ class FaceRecognition(Resource):
     def get(self, img_name):   
         for img in knowledge_base: # FaceRecognicion
             result = DeepFace.verify(img1_path = f'img/{img_name}', img2_path = f'names_img/{img}')
-            print(result)
+            print(f'Result JSON:\n{result}')
             if result['verified'] == True:
                 df = pd.read_csv('names.csv')
                 df = df[df['IMG'] == f'{img}']
                 name = {'name':df['NAME'].to_string(header=False, index=False)}
                 json_object = json.dumps(name, indent = 4) 
-                print(json_object) 
+                print(f'json_object:\n{json_object}') 
                 return json_object
         name = {'name':'not found'}
         json_object = json.dumps(name, indent = 4)
@@ -54,6 +68,7 @@ class AddName(Resource):
         df = df.append(new_entry, ignore_index=True)
         df.to_csv('names.csv', index=False)
         # Move img to dir with known names
+        # moveFile()
         return {'data': 'Posted'}
 
 # add this resource to the api and make it acessable through URL
