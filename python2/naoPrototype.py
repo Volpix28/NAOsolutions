06bafa6173c5b557e2afbf20cfaf9b1cb19078c1
KoHias <matthias.kopeinig@gmx.at>
2022-11-20 12:38:24 +0100

    takePicture function finalized and tested with NAO

--- python2/naoPrototype.py
+++ python2/naoPrototype.py
@@ -20,48 +20,13 @@ tts = 'ALTextToSpeech'
 text = ALProxy(tts, NAOIP, PORT)
 text.say('Testrun')
 
-def takePicture(IP, PORT, camera, resolution, colorSpace): # see takePictureNew - matthias
-  '''
-  First get an image from Nao, then show it on the screen with PIL.
-  '''
-  camProxy = ALProxy('ALVideoDevice', IP, PORT)
-  videoClient = camProxy.subscribeCamera('python_client', 0, resolution, colorSpace, 5)
-  t0 = time.time()
-
-  # Get a camera image.
-  # image[6] contains the image data passed as an array of ASCII chars.
-  naoImage = camProxy.getImageRemote(videoClient)
-  t1 = time.time()
-  
-  # Time the image transfer.
-  print('acquisition delay ', t1-t0)
-  camProxy.unsubscribe(videoClient)
-
-  # Now we work with the image returned and save it as a PNG  using ImageDraw package.
-
- 
-  # Get the image size and pixel array.
-  imageWidth = naoImage[0]
-  imageHeight = naoImage[1]
-  array = naoImage[6]
+# NAO picture config
+camera = 1 # 0 = top camera, 1 = bottom camera
+resolution = 3 # 0 = QQVGA, 1 = QVGA, 2 = VGA
+colorSpace = 11 # http://doc.aldebaran.com/2-5/family/robots/video_robot.html#cameracolorspace-mt9m114
 
 
-  # Create a PIL Image from our pixel array.
-  im = Image.frombytes('RGB', (imageWidth, imageHeight), array)
-
-  # Save the image.
-  current_GMT = time.gmtime()
-
-  time_stamp = calendar.timegm(current_GMT)
-  time_stamp = str(time_stamp)
-  
-  global imName
-  imName = r'image_' + time_stamp + r'.png'
-  im.save('fileshare' + os.sep + imName, 'PNG') #Save files with continuative numbers
-
-
-# TODO: needs to be tested with NAO - matthias
-def takePictureNew(IP, PORT, camera, resolution, colorSpace, location):
+def takePicture(IP, PORT, camera, resolution, colorSpace, location):
   camProxy = ALProxy('ALVideoDevice', IP, PORT)
   videoClient = camProxy.subscribeCamera('python_client', camera, resolution, colorSpace, 5)
   naoImage = camProxy.getImageRemote(videoClient)
@@ -72,18 +37,13 @@ def takePictureNew(IP, PORT, camera, resolution, colorSpace, location):
   print('Image: ' + imageName + ' successfully saved @ ' + location)
   return imageName
 
-  
-camera = 1 # 0 = top camera, 1 = bottom camera
-resolution = 3 # 0 = QQVGA, 1 = QVGA, 2 = VGA
-colorSpace = 11 # http://doc.aldebaran.com/2-5/family/robots/video_robot.html#cameracolorspace-mt9m114
-# naoImage = takePicture(NAOIP, PORT, camera, resolution, colorSpace)
-naoImage = takePictureNew(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare')
+naoImage = takePicture(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare') # TODO set fileshare param to standard location 
 
-#Filler
+# Filler
 text.say('I hope you are having a good day.')
 
-response_fr = requests.get(BASE_API + 'facerecognition/' + imName) # .get, .post
-response_ed = requests.get(BASE_API + 'emotiondetection/' + imName)
+response_fr = requests.get(BASE_API + 'facerecognition/' + naoImage) # .get, .post
+response_ed = requests.get(BASE_API + 'emotiondetection/' + naoImage)
 result_ed = response_ed.json()
 result_fr = ast.literal_eval(response_fr.json())
 gender = str(result_ed[u'gender'])
