e8a9f82befdad27de3228c3437c870caeaccef1e
mdielacher <dielacher@gmx.at>
2022-11-22 15:49:02 +0100

    Phrases for Speech2Text

--- python2/dialog.py
+++ python2/dialog.py
@@ -1,3 +1,6 @@
 class Dialog:
-    test = 'hallo'
-    
+    greetings = ['hallo', 'hello']
+    say_name = 'Please say your name now.'
+    sorry_message = ["Sorry, i didn't understand you, try again please!", "Sorry, i didn't understand you!", 'Okay, i am sorry for that, please say your Name now.']
+    conformation_message = ['Your Name is ', ' im right?', ' Please say yes or no!', 'Okay! ', ' I will enter your Name into the knowledge data base.']
+
