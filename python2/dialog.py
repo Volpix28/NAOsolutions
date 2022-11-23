930ff86a08ce43eb92958720bf27a34d092e46d7
mdielacher <dielacher@gmx.at>
2022-11-23 16:09:16 +0100

    Update dialog.py

--- python2/dialog.py
+++ python2/dialog.py
@@ -3,4 +3,3 @@ class Dialog:
     say_name = 'Please say your name now.'
     sorry_message = ["Sorry, i didn't understand you, try again please!", "Sorry, i didn't understand you!", 'Okay, i am sorry for that, please say your Name now.']
     conformation_message = ['Your Name is ', ' im right?', ' Please say yes or no!', 'Okay! ', ' I will enter your Name into the knowledge data base.']
-
