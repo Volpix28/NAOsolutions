1919c0395282792beb8146830ee11e6fc16b6079
KoHias <matthias.kopeinig@gmx.at>
2022-12-01 17:48:33 +0100

    several API optimizations/fixes, removing of unnecessary files (test pngs, emtpy test notebooks) requirement.txt at wrong location

--- python3/main.py
+++ python3/main.py
@@ -4,13 +4,12 @@ from deepface import DeepFace
 from functools import partial
 import pandas as pd
 import os
-import json
 
 
 def moveFile(source, dest):
     '''
     Move single file from source to destination.
-    source: filepath + filename 
+    source: filepath + filename
     dest: new filepath + (new) filename
     '''
     try:
@@ -26,7 +25,7 @@ def createFolders(dir, subfolders):
     subfolders: name(s) of folder(s)
     '''
     concat_path = partial(os.path.join, dir) # creates local temp function to map each concatenated subfolder
-    for subfolder in map(concat_path, subfolders): 
+    for subfolder in map(concat_path, subfolders):
         os.makedirs(subfolder, exist_ok=True)
 
 def createNamesCsv(dir):
@@ -51,68 +50,39 @@ fileshare = os.path.join(os.getcwd(), 'fileshare')
 createNamesCsv(fileshare)
 names_csv = os.path.join(fileshare + os.sep + 'names.csv')
 knowledge_base = os.path.join(fileshare, 'knowledge_base')
-knowledge_base_images = os.listdir(knowledge_base)
 images_folder = os.path.join(fileshare, 'images')
 
 
 class EmotionDetection(Resource):
     def get(self, img_name):
-        try:
-            obj = DeepFace.analyze(images_folder + os.sep + img_name, actions = ['gender', 'emotion'])
-            obj = {'dominant_emotion':obj['dominant_emotion'], 'gender':obj['gender']}
-            json_object = json.dumps(obj, indent = 4)
-            return json_object
-        except:
-            obj = json.dumps({'dominant_emotion': 'face_not_found'}, indent = 4)
-            return obj
+        obj = DeepFace.analyze(images_folder + os.sep + img_name, actions = ['gender', 'emotion'])
+        obj = {'dominant_emotion':obj['dominant_emotion'], 'gender':obj['gender']} # filter dominant_emotion and gender
+        return obj
 
-'''
-# NEW
+
 class FaceRecognition(Resource):
-    def get(self, img_name):   
-        result = DeepFace.verify(images_folder + os.sep + img_name, knowledge_base + os.sep + img_name)
-        if result['verified'] == True:
-            df = pd.read_csv(names_csv)
-            df = df[df['IMG'] == f'{img_name}']
-            name = {'name':df['NAME'].to_string(header=False, index=False)}
-            json_object = json.dumps(name, indent = 4)
-            print(f'json_object:\n{json_object}')
-        else:
-            name = {'name':'not found'}
-            json_object = json.dumps(name, indent = 4)
-        return json_object
-'''
-
-# OLD
-class FaceRecognition(Resource):
-    def get(self, img_name):   
-        if knowledge_base_images:
-            for img in knowledge_base_images:
-                result = DeepFace.verify(images_folder + os.sep + img_name, knowledge_base + os.sep + img, model_name='Facenet512', distance_metric='euclidean_l2')
-                print(f'Result JSON:\n{result}')
-                if result['verified'] == True:
-                    df = pd.read_csv(names_csv)
-                    df = df[df['IMG'] == f'{img}']
-                    print(df)
-                    name = {'name':df['NAME'].to_string(header=False, index=False), 'img_id':df['IMG'].to_string(header=False, index=False)}
-                    json_object = json.dumps(name, indent = 4)
-                    print(f'json_object:\n{json_object}')
-                    return json_object
-                
-            name = {'name':'not_found', 'img_id':'not_in_database'}
-            json_object = json.dumps(name, indent = 4)
-            return json_object
-        else: #is needed when knownledge base is empty
-            name = {'name':'not_found', 'img_id':'not_in_database'}
-            json_object = json.dumps(name, indent = 4)
-            return json_object
-
+    def get(self, img_name):
+        name, img_id = 'not found', 'not_in_database'
+        known_images = os.listdir(knowledge_base)
+        for i in range(0, len(known_images)):
+            result = DeepFace.verify(img1_path=knowledge_base + os.sep + known_images[i],
+                                     img2_path=images_folder + os.sep + img_name,
+                                     model_name='Facenet512',
+                                     distance_metric='euclidean_l2')
+            print(f'\nINFO - FaceRecognition: {i+1}. run: comparing image \"{img_name}\" with \"{known_images[i]}\"\nresult: {result}\n\n')
+            if result['verified'] == True:
+                df = pd.read_csv(names_csv)
+                name = df['NAME'][df['IMG']==known_images[i]].values[0]
+                img_id = known_images[i]
+                break
+        os.remove(images_folder + os.sep + img_name)
+        return {'name': name, 'img_id': img_id}
 
 
 class AddName(Resource):
     def get(self, person_name, img_name):
         '''
-        adds image name and name of person to names.csv and moves the png file from 'images' to 'knowledge_base'
+        adding image name and name of person to names.csv and moves the file from 'images' to 'knowledge_base'
         '''
         df = pd.read_csv(names_csv)
         new_entry = {'IMG' : img_name, 'NAME': person_name}
@@ -127,16 +97,16 @@ class DeletePerson(Resource):
         '''
         remove image from folder \'images\', \'knowledge_base\' and names.csv file
         '''
-        print(f'test = {img_id}')
         df = pd.read_csv(names_csv)
-        file_name = df.loc[df['IMG'] == img_id, 'IMG'].item()
         df.drop(df.loc[df['IMG']==img_id].index, inplace=True)
         df.to_csv(names_csv, index=False)
-        knowledge_base_file = os.path.join(knowledge_base + os.sep + file_name)
+        knowledge_base_file = os.path.join(knowledge_base + os.sep + img_id)
+        images_file = os.path.join(images_folder + os.sep + img_id)
         os.remove(knowledge_base_file)
-        print(f'Knowledge base file: \"{knowledge_base_file}\" deleted')
+        print(f'INFO - DeletePerson: \"{knowledge_base_file}\" deleted')
+        os.remove(images_file)
+        print(f'INFO - DeletePerson: \"{images_file}\" deleted')
         return {'data': 'deleted'}
-
 
 
 # add this resource to the api and make it accessable through URL
@@ -147,4 +117,4 @@ api.add_resource(DeletePerson, "/deleteperson/<string:img_id>")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True) # set debug=False in production
+    app.run(host='0.0.0.0', debug=True)
