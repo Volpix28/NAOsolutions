b54b2182a9001831e3ab3a7e856face17b365fd3
KoHias <matthias.kopeinig@gmx.at>
2022-12-01 20:05:53 +0100

    code format changes to pep8

--- python3/main.py
+++ python3/main.py
@@ -18,15 +18,17 @@ def moveFile(source, dest):
     except FileNotFoundError as err:
         print(f'ERROR: {err}')
 
+
 def createFolders(dir, subfolders):
     '''
     Creates one or more directories if not existent.
     dir: absolute path
     subfolders: name(s) of folder(s)
     '''
-    concat_path = partial(os.path.join, dir) # creates local temp function to map each concatenated subfolder
+    concat_path = partial(os.path.join, dir)  # local temp function to map concatenated subfolder
     for subfolder in map(concat_path, subfolders):
         os.makedirs(subfolder, exist_ok=True)
+
 
 def createNamesCsv(dir):
     '''
@@ -36,7 +38,7 @@ def createNamesCsv(dir):
     if os.path.exists(file_name):
         print('INFO - createNamesCsv: names.csv already exists')
     else:
-        df = pd.DataFrame(columns=['IMG','NAME'])
+        df = pd.DataFrame(columns=['IMG', 'NAME'])
         df.to_csv(file_name, index=False)
         print('INFO - createNamesCsv: names.csv created')
 
@@ -55,8 +57,8 @@ images_folder = os.path.join(fileshare, 'images')
 
 class EmotionDetection(Resource):
     def get(self, img_name):
-        obj = DeepFace.analyze(images_folder + os.sep + img_name, actions = ['gender', 'emotion'])
-        obj = {'dominant_emotion':obj['dominant_emotion'], 'gender':obj['gender']} # filter dominant_emotion and gender
+        obj = DeepFace.analyze(images_folder + os.sep + img_name, actions=['gender', 'emotion'])
+        obj = {'dominant_emotion': obj['dominant_emotion'], 'gender': obj['gender']}  # filter dominant_emotion and gender
         return obj
 
 
@@ -70,7 +72,7 @@ class FaceRecognition(Resource):
                                      model_name='Facenet512',
                                      distance_metric='euclidean_l2')
             print(f'\nINFO - FaceRecognition: {i+1}. run: comparing image \"{img_name}\" with \"{known_images[i]}\"\nresult: {result}\n\n')
-            if result['verified'] == True:
+            if result['verified'] is True:
                 df = pd.read_csv(names_csv)
                 name = df['NAME'][df['IMG']==known_images[i]].values[0]
                 img_id = known_images[i]
@@ -85,7 +87,7 @@ class AddName(Resource):
         adding image name and name of person to names.csv and moves the file from 'images' to 'knowledge_base'
         '''
         df = pd.read_csv(names_csv)
-        new_entry = {'IMG' : img_name, 'NAME': person_name}
+        new_entry = {'IMG': img_name, 'NAME': person_name}
         df = df.append(new_entry, ignore_index=True)
         df.to_csv(names_csv, index=False)
         moveFile(images_folder + os.sep + img_name, knowledge_base + os.sep + img_name)
@@ -110,7 +112,7 @@ class DeletePerson(Resource):
 
 
 # add this resource to the api and make it accessable through URL
-api.add_resource(EmotionDetection, "/emotiondetection/<string:img_name>") # add parameters with /<int:test>/...
+api.add_resource(EmotionDetection, "/emotiondetection/<string:img_name>")  # add parameters with /<int:test>/...
 api.add_resource(FaceRecognition, "/facerecognition/<string:img_name>")
 api.add_resource(AddName, "/addname/<string:person_name>/<string:img_name>")
 api.add_resource(DeletePerson, "/deleteperson/<string:img_id>")
