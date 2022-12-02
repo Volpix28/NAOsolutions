5cfed16ee9e427ffcfa4e5f3cd07e42ea9230675
mdielacher <dielacher@gmx.at>
2022-12-02 18:52:32 +0100

    made a class for Connection settings

--- python2/main.py
+++ python2/main.py
@@ -20,15 +20,16 @@ Functions = Functions()
 
 
 # Connection settings
-NAOIP = '192.168.0.242'
-PORT = 9559
-NAME = "nao"
-passwd = "19981"
-BASE_API = 'http://192.168.0.213:5000'
+class Connection_settings:
+    NAOIP = '192.168.0.242'
+    PORT = 9559
+    NAME = "nao"
+    passwd = "19981"
+    BASE_API = 'http://192.168.0.213:5000'
 
 # Test
 tts = 'ALTextToSpeech'
-text = ALProxy(tts, NAOIP, PORT)
+text = ALProxy(tts, Connection_settings.NAOIP, Connection_settings.PORT)
 text.setParameter("speed", 80)
 
 
@@ -42,27 +43,27 @@ colorSpace = 11 # http://doc.aldebaran.com/2-5/family/robots/video_robot.html#ca
 ##########################
 
 text.say(Dialog.welcome)
-naoImage = Functions.takePicture(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare/images') 
+naoImage = Functions.takePicture(Connection_settings.NAOIP, Connection_settings.PORT, camera, resolution, colorSpace, 'fileshare/images') 
 time.sleep(2)
-response_ed = requests.get(BASE_API + '/emotiondetection/' + naoImage)
+response_ed = requests.get(Connection_settings.BASE_API + '/emotiondetection/' + naoImage)
 result_ed = ast.literal_eval(response_ed.json())
 while result_ed['dominant_emotion'] == 'face_not_found':
     # TO-DO: delete old naoImage from fileshare
     text.say('Face not found.')
-    naoImage = Functions.takePicture(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare/images')
-    response_ed = requests.get(BASE_API + '/emotiondetection/' + naoImage)
+    naoImage = Functions.takePicture(Connection_settings.NAOIP, Connection_settings.PORT, camera, resolution, colorSpace, 'fileshare/images')
+    response_ed = requests.get(Connection_settings.BASE_API + '/emotiondetection/' + naoImage)
     result_ed = ast.literal_eval(response_ed.json())
     
-response_fr = requests.get(BASE_API + '/facerecognition/' + naoImage)
+response_fr = requests.get(Connection_settings.BASE_API + '/facerecognition/' + naoImage)
 result_fr = ast.literal_eval(response_fr.json())
 gender = str(result_ed[u'gender'])
 emotion = str(result_ed[u'dominant_emotion'])
 
 if result_fr['name'] == 'not_found':
     text.say(Dialog.name_question(gender))
-    name = Functions.get_and_save_name(NAOIP, PORT)
+    name = Functions.get_and_save_name(Connection_settings.NAOIP, Connection_settings.PORT)
     img_id = result_fr['img_id']
-    requests.get(BASE_API + '/addname/' + name + '/' + naoImage)
+    requests.get(Connection_settings.BASE_API + '/addname/' + name + '/' + naoImage)
 else:
     name = result_fr['name']
     img_id = result_fr['img_id']
@@ -79,16 +80,16 @@ Functions.action(manual_emotion_rating, emotion)
 
 #Take another picture to check if mood changed
 #Change variable names or no need?
-naoImage = Functions.takePicture(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare/images') 
+naoImage = Functions.takePicture(Connection_settings.NAOIP, Connection_settings.PORT, camera, resolution, colorSpace, 'fileshare/images') 
 
 #Emotion detection
-response_ed = requests.get(BASE_API + '/emotiondetection/' + naoImage)
+response_ed = requests.get(Connection_settings.BASE_API + '/emotiondetection/' + naoImage)
 result_ed = ast.literal_eval(response_ed.json())
 while result_ed['dominant_emotion'] == 'face_not_found':
     # TO-DO: delete old naoImage from fileshare
     text.say('Face not found.')
-    naoImage = Functions.takePicture(NAOIP, PORT, camera, resolution, colorSpace, 'fileshare/images')
-    response_ed = requests.get(BASE_API + '/emotiondetection/' + naoImage)
+    naoImage = Functions.takePicture(Connection_settings.NAOIP, Connection_settings.PORT, camera, resolution, colorSpace, 'fileshare/images')
+    response_ed = requests.get(Connection_settings.BASE_API + '/emotiondetection/' + naoImage)
     result_ed = ast.literal_eval(response_ed.json())
 
 emotion2 = str(result_ed[u'dominant_emotion'])
